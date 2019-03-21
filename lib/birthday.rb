0bbb77be4f53623b3b18523f818e451b717a77d0
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-21 20:19:10 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,4 +11,13 @@ def happy_birthday(birthday_kids)
   end
 end
 
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |name, age|
+    if age <= 12
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    else 
+      puts 'You are too old for this.'
+    end
+  end
+end
 
