78ae2de96e1992c1999ebac155cbf1963aa8635b
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-21 20:14:01 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -14,3 +14,13 @@ def select_winner(passengers)
     end
   end
 end
+
+def select_winner(passengers)
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+  end
+end
+  winner
+end
